b307fbf48cd669ec94bd05368e1080f383ccedef
Adam Stambler <astambler@willowgarage.com>
2011-06-23 14:34:29 -0700

    Changed how the rosserial_arduino msgs are generated. 	Now  msgs have only header files.
    rosserial_python:
    	serial_node no longer blocks inside constructor

--- serial_fx.h
+++ serial_fx.h
@@ -2,17 +2,23 @@
  * Redefine these if needed
  */
 
+#ifndef serial_fx_H
+#define serial_fx_H
+
 #include "WProgram.h"
 
 void fx_open(){
     Serial.begin(57600);
 }
 
-inline int fx_putc(char c) {
+
+ int fx_putc(char c) {
     Serial.write(c);
     return 0;
 }
-
-inline int fx_getc(){
+ 
+ int fx_getc(){
     return Serial.read();
 }
+
+#endif
