eb59e10054123bed775a9aec3d958efe959c1ae1
josh <josh@tesseractmobile.com>
2020-12-03 21:35:52 -0600

    Added file copying to update script

--- update.sh
+++ update.sh
@@ -9,3 +9,15 @@ docker cp ros_serial_update:/tmp/ros_lib ./
 docker stop ros_serial_update
 
 docker rm ros_serial_update
+
+cp -Rf ./ros_lib/* ./src
+
+cp -Rf ./ros_lib/examples/* ./examples
+
+cp -Rf ./ros_lib/tests/* ./extras/tests
+
+rm -Rf ./src/examples
+
+rm -Rf ./src/tests
+
+rm -Rf ./ros_lib
